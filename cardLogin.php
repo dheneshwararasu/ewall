bcd306779c8771d416e8d5e18a7cb1aed5835a50
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-02-02 02:21:27 +0800

    Update cardLogin.php

--- cardLogin.php
+++ cardLogin.php
@@ -4,7 +4,10 @@
         <title>WALL-E Kiosk | INNOVUS</title>
     </head>
     <body>
-        <?php $output= exec("py/rfid/readCard.py"); ?>
+        <?php
+        $command = escapeshellcmd('python3 py/rfid/readCard.py'); 
+        $output= shell_exec("$command"); 
+        ?>
         <h1><?php echo $output; ?></h1>
     </body>
 </html>
