d03d9e8393fdc3033f6a991067ceaa0a00609e06
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-02-01 23:43:43 +0800

    updates

--- cardLogin.php
+++ cardLogin.php
@@ -4,7 +4,6 @@
         <title>WALL-E Kiosk | INNOVUS</title>
     </head>
     <body>
-        <h1>Let's make the change now</h1>
-        <button href="cardLogin.php" >Get Started!</button>
+        <h1>Please tap your TP card...</h1>
     </body>
 </html>
