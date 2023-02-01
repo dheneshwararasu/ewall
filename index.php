22dd0d89dec400542e5a5732ef3094840046b3ff
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-02-02 00:42:41 +0800

    update

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="css/styles.css">
     </head>
     <body>
+        <div class="bg-image"></div>
         <h1 class="center">Let's make the change now</h1>
         <a href="cardLogin.php" >
             <button>Get Started!</button>
