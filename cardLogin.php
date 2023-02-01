a5995fec33485a4087290c97867379aa9bfe12bb
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-02-02 02:31:56 +0800

    Update cardLogin.php

--- cardLogin.php
+++ cardLogin.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <?php
-        $command = escapeshellcmd('py/rfid/readCard.py'); 
+        $command = escapeshellcmd('python py/rfid/readCard.py'); 
         $output= shell_exec("$command");
         echo $output;
         ?>
