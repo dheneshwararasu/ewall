78e35fa0a2f7a0937eff824077f3e42e6685dc24
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-02-01 23:29:31 +0800

    Update index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WALL-E Kiosk | INNOVUS</title>
+    </head>
+    <body>
+        <h1>Let's make the change now</h1>
+        <button>Get Started!</button>
+    </body>
+</html>
