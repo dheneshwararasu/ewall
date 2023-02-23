3a1734794b62984d10ab09bca138c7b064ea640b
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-02-24 03:06:33 +0800

    Update disposalStatus.php

--- disposalStatus.php
+++ disposalStatus.php
@@ -2,10 +2,8 @@
 include("conn.php");
 session_start();
 
-shell_exec("./servo_py/first.py");
-
 if ($_POST['action'] == 'Perfect!') {
-    exec("python3 servo_py/first.py")
+    exec("sudo python3 servo_py/first.py")
     $newURL = 'disposalComplete.php?id=';
 	header('Location: '.$newURL.$id);
 } else if ($_POST['action'] == 'No, this is not!') {
