e6bac11648e72095f54d88145c9d2022eabd8cb6
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-02-24 01:20:41 +0800

    Update first.py

--- servo_py/first.py
+++ servo_py/first.py
@@ -2,6 +2,7 @@
 
 from gpiozero import LED
 from time import sleep
+import sys
 
 led = LED("GPIO17")
 
@@ -9,6 +10,9 @@ while True:
     led.on()
     sleep(2)
     led.off()
+    sleep(1)
+
+    pause()
 
 GPIO.cleanup()
 sys.exit(0)
