e0f5f6bf3cd0376c0aa140d285887a94cb528e79
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-02-23 22:22:32 +0800

    Update disposalStatus.php

--- disposalStatus.php
+++ disposalStatus.php
@@ -3,6 +3,7 @@ include("conn.php");
 session_start();
 
 if ($_POST['action'] == 'Perfect!') {
+    
     $newURL = 'disposalComplete.php?id=';
 	header('Location: '.$newURL.$id);
 } else if ($_POST['action'] == 'No, this is not!') {
