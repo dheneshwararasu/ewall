f68c751e2ee8dc4ab801054495f0433555fdbbad
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-02-23 23:49:09 +0800

    up

---
+++ servo motor control with sensor/first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+
+led = 18
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led, GPIO.OUT)
+
+GPIO.output(led, GPIO.HIGH)
+time.sleep(0.2)
+GPIO.output(led, GPIO.LOW)
+time.sleep(0.2)
+print('Switch status = ', GPIO.output(led))
+
+GPIO.cleanup()
