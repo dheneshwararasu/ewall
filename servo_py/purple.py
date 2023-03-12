71287fba8f37068a5afc659f4028c6148e0fa4fa
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-03-13 01:33:52 +0800

    Update purple.py

--- servo_py/purple.py
+++ servo_py/purple.py
@@ -9,7 +9,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servo, GPIO.OUT)
 
 if True:
-        GPIO.output(servo, GPIO.HIGH)
+    GPIO.output(servo, GPIO.HIGH)
 	time.sleep(1)
 	GPIO.output(servo, GPIO.LOW)
 	time.sleep(1)
